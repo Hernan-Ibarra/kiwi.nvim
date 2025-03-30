a1397b0a18c8c230c214fad4a5219581ae8c30ee
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-03-30 20:04:42 +0100

    More idiomatic lua

--- lua/kiwi/utils.lua
+++ lua/kiwi/utils.lua
@@ -1,13 +1,13 @@
-local utils = {}
+local M = {}
 
 -- Setup wiki folder
-utils.setup = function(opts, config)
-  if opts ~= nil then
+M.setup = function(opts, config)
+  if opts then
     config.folders = opts
   else
-    config.path = utils.get_wiki_path()
+    config.path = M.get_wiki_path()
   end
-  utils.ensure_directories(config)
+  M.ensure_directories(config)
 end
 
 local create_dirs = function(wiki_path)
@@ -16,24 +16,24 @@ local create_dirs = function(wiki_path)
 end
 
 -- Get the default Wiki folder path
-utils.get_wiki_path = function()
+M.get_wiki_path = function()
   local default_dir = vim.fs.joinpath(vim.loop.os_homedir(), "wiki")
   return default_dir
 end
-
 -- Create wiki folder
-utils.ensure_directories = function(config)
-  if (config.folders ~= nil) then
-    for _, props in ipairs(config.folders) do
-      create_dirs(props.path)
-    end
-  else
+M.ensure_directories = function(config)
+  if not config.folders then
     create_dirs(config.path)
+    return
+  end
+
+  for _, props in ipairs(config.folders) do
+    create_dirs(props.path)
   end
 end
 
 -- Check if the cursor is on a link on the line
-utils.is_link = function(cursor, line)
+M.is_link = function(cursor, line)
   cursor[2] = cursor[2] + 1 -- because vim counts from 0 but lua from 1
 
   -- Pattern for [title](file)
@@ -43,7 +43,7 @@ utils.is_link = function(cursor, line)
     local match_start, match_end, _, file = line:find(pattern1, start_pos)
     if not match_start then break end
     start_pos = match_end + 1 -- Move past the current match
-    file = utils._is_cursor_on_file(cursor, file, match_start, match_end)
+    file = M._is_cursor_on_file(cursor, file, match_start, match_end)
     if file then return file end
   end
 
@@ -54,7 +54,7 @@ utils.is_link = function(cursor, line)
     local match_start, match_end, file = line:find(pattern2, start_pos)
     if not match_start then break end
     start_pos = match_end + 1 -- Move past the current match
-    file = utils._is_cursor_on_file(cursor, file, match_start, match_end)
+    file = M._is_cursor_on_file(cursor, file, match_start, match_end)
     if file then return file end
   end
 
@@ -62,7 +62,7 @@ utils.is_link = function(cursor, line)
 end
 
 -- Private function to determine if cursor is placed on a valid file
-utils._is_cursor_on_file = function(cursor, file, match_start, match_end)
+M._is_cursor_on_file = function(cursor, file, match_start, match_end)
   if cursor[2] >= match_start and cursor[2] <= match_end then
     if not file:match("%.md$") then
       file = file .. ".md"
@@ -71,7 +71,7 @@ utils._is_cursor_on_file = function(cursor, file, match_start, match_end)
   end
 end
 
-utils.choose_wiki = function(folders)
+M.choose_wiki = function(folders)
   local path = ""
   local list = {}
   for i, props in pairs(folders) do
@@ -93,16 +93,16 @@ utils.choose_wiki = function(folders)
 end
 
 -- Show prompt if multiple wiki path found or else choose default path
-utils.prompt_folder = function(config)
+M.prompt_folder = function(config)
   if config.folders ~= nil then
     local count = 0
     for _ in ipairs(config.folders) do count = count + 1 end
     if count > 1 then
-      config.path = utils.choose_wiki(config.folders)
+      config.path = M.choose_wiki(config.folders)
     else
       config.path = config.folders[1].path
     end
   end
 end
 
-return utils
+return M
